073cbc45dd5f1b65c692cbffaa368fd63f1a00e2
Matt <carlotta.matt@gmail.com>
2021-07-02 13:36:10 -0700

    [core]: Rename redux state

--- ComponentBranch/index.tsx
+++ ComponentBranch/index.tsx
@@ -2,24 +2,19 @@ import * as React from "react";
 import { connect } from "react-redux";
 
 export const ComponentBranch = (
-  testFunc: (toggles: any) => boolean,
+  testFunc: (state: any) => boolean,
   ComponentA: React.ComponentType<any>,
   ComponentB: React.ComponentType<any>
 ): React.ComponentType<any> => {
-  const WithToggleComponent = ({
-    toggles,
-    ...rest
-  }: any): React.ReactElement => {
-    return testFunc(toggles) ? (
+  const WithToggleComponent = ({ state, ...rest }: any): React.ReactElement => {
+    return testFunc(state) ? (
       <ComponentA {...rest} />
     ) : (
       <ComponentB {...rest} />
     );
   };
 
-  const mapStateToProps = (state: any) => ({
-    toggles: state
-  });
+  const mapStateToProps = (state: any) => ({ state });
 
   return connect<ReturnType<typeof mapStateToProps>>(mapStateToProps)(
     WithToggleComponent
