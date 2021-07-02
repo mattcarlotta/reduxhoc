60705dcd19ab965bdb6e8a11a7613678b1d74c1b
Matt <carlotta.matt@gmail.com>
2021-07-02 13:48:29 -0700

    [core]: Update jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "testEnvironment": "node",
+  "moduleDirectories": ["<rootDir>", "node_modules"],
+  "transform": {
+    "^.+\\.(ts|js)x?$": "ts-jest"
+  },
+  "testMatch": ["**/*.spec.ts"],
+  "transformIgnorePatterns": ["^.+\\.js$"],
+  "testPathIgnorePatterns": [
+    "<rootDir>/coverage",
+    "<rootDir>/esm",
+    "<rootDir>/node_modules",
+    "<rootDir>/types",
+    "<rootDir>/utils"
+  ],
+  "collectCoverageFrom": ["**/*.ts", "!**/*d.ts", "!utils/*.ts"]
+}
